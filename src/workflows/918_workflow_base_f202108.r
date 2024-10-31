c93f471fe9c76b34b34fba6088f161563e1bbc9f
Brenda Quinhonez <brenda.quinnonez@gmail.com>
2024-10-31 04:29:20 +0000

    Cambio de parámetros de canaritos
    Signed-off-by: Brenda Quinhonez <brenda.quinnonez@gmail.com>

--- src/workflows/918_workflow_base_f202108.r
+++ src/workflows/918_workflow_base_f202108.r
@@ -439,7 +439,7 @@ wf_agosto <- function( pnombrewf )
   #  mtry_ratio= 0.2
   #)
 
-  CN_canaritos_asesinos_base(ratio=0.2, desvio=4.0)
+  CN_canaritos_asesinos_base(ratio=0.5, desvio=1.0)
 
   # Etapas modelado
   ts8 <- TS_strategy_base8()

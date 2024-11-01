cb4461b69007a2bf9c2b60bbc04daa590c78abd4
Brenda Quinhonez <brenda_quinnonez@canaritos-asesinos.us-west4-a.c.deft-orb-436002-f6.internal>
2024-11-01 17:41:18 +0000

    Junio Canarios
    Signed-off-by: Brenda Quinhonez <brenda_quinnonez@canaritos-asesinos.us-west4-a.c.deft-orb-436002-f6.internal>

--- src/workflows/918_workflow_base_f202108.r
+++ src/workflows/918_workflow_base_f202108.r
@@ -267,18 +267,19 @@ TS_strategy_base8 <- function( pinputexps )
   param_local$meta$script <- "/src/wf-etapas/z2101_TS_training_strategy.r"
 
 
-  param_local$future <- c(202108)
-
+  param_local$future <- c(202106)
+  
   param_local$final_train$undersampling <- 1.0
   param_local$final_train$clase_minoritaria <- c( "BAJA+1", "BAJA+2")
-  param_local$final_train$training <- c(202106, 202105, 202104,
-    202103, 202102, 202101)
+  param_local$final_train$training <- c(202104, 202103, 202102,
+                                        202101, 202012, 202011)
+  
+  
+  param_local$train$training <- c(202102, 202101, 202012,
+                                  202111, 202010, 202009)
+  param_local$train$validation <- c(202103)
+  param_local$train$testing <- c(202104)
 
-
-  param_local$train$training <- c(202104, 202103, 202102,
-    202101, 202012, 202011)
-  param_local$train$validation <- c(202105)
-  param_local$train$testing <- c(202106)
 
   # Atencion  0.2  de  undersampling de la clase mayoritaria,  los CONTINUA
   # 1.0 significa NO undersampling
@@ -420,7 +421,7 @@ KA_evaluate_kaggle <- function( pinputexps )
 # Este es el  Workflow Baseline
 # Que predice 202108 donde NO conozco la clase
 
-wf_agosto <- function( pnombrewf )
+wf_junio_canarios <- function( pnombrewf )
 {
   param_local <- exp_wf_init( pnombrewf ) # linea workflow inicial fija
 
@@ -457,5 +458,5 @@ wf_agosto <- function( pnombrewf )
 # Aqui comienza el programa
 
 # llamo al workflow con future = 202108
-wf_agosto()
+wf_junio_canarios()
 

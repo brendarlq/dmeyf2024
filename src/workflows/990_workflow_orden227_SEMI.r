579a95731211e32ab6d1c92996225fa911fe77e0
Brenda Quinhonez <brenda.quinnonez@gmail.com>
2024-11-22 23:54:01 +0000

    Semillero con cambio de meses
    Signed-off-by: Brenda Quinhonez <brenda.quinnonez@gmail.com>

--- src/workflows/990_workflow_orden227_SEMI.r
+++ src/workflows/990_workflow_orden227_SEMI.r
@@ -151,9 +151,9 @@ FEhist_base <- function( pinputexps)
   param_local$Tendencias1$ratiomax <- FALSE
 
   # no me engraso las manos con las tendencias de segundo orden
-  param_local$Tendencias2$run <- FALSE
+  param_local$Tendencias2$run <- TRUE
   param_local$Tendencias2$ventana <- 12
-  param_local$Tendencias2$tendencia <- FALSE
+  param_local$Tendencias2$tendencia <- TRUE
   param_local$Tendencias2$minimo <- FALSE
   param_local$Tendencias2$maximo <- FALSE
   param_local$Tendencias2$promedio <- FALSE
@@ -299,9 +299,12 @@ TS_strategy_base8 <- function( pinputexps )
   param_local$final_train$undersampling <- 0.02
   param_local$final_train$clase_minoritaria <- c( "BAJA+1", "BAJA+2")
   param_local$final_train$training <- c(
-    202106, 202105, 202104, 202103, 202102, 202101, 
-    202012, 202011, 202010, 202009, 202008, 202007, 
-    # 202006  Excluyo por variables rotas
+    202106, 202105, 
+    # 202104, 202103, 202102, 
+    202101, 
+    202012, 202011, 
+    202010, 
+    202009, 202008, 202007, 202006, 
     202005, 
     # 202004, 202003, 
     202002, 202001,
@@ -317,9 +320,11 @@ TS_strategy_base8 <- function( pinputexps )
   param_local$train$validation <- c(202105)
 
   param_local$train$training <- c(
-    202104, 202103, 202102, 202101, 
-    202012, 202011, 202010, 202009, 202008, 202007, 
-    # 202006  Excluyo por variables rotas
+    # 202104, 202103, 202102, 
+    202101, 
+    202012, 202011, 
+    202010, 
+    202009, 202008, 202007, 202006, 
     202005, 
     # 202004, 202003, 
     202002, 202001,
@@ -480,7 +485,7 @@ KA_evaluate_kaggle_semillerio <- function( pinputexps )
 # Que predice 202107 donde conozco la clase
 # y ya genera graficos
 
-wf_SEMI_ago_orden227 <- function( pnombrewf )
+wf_SEMI_ago_orden2 <- function( pnombrewf )
 {
   param_local <- exp_wf_init( pnombrewf ) # linea fija
 
@@ -493,14 +498,14 @@ wf_SEMI_ago_orden227 <- function( pnombrewf )
   FEhist_base()
   ultimo <- FErf_attributes_base()
   
-  CN_canaritos_asesinos_base(ratio=0.6, desvio=0)
+  #CN_canaritos_asesinos_base(ratio=0.6, desvio=0)
 
   ts8 <- TS_strategy_base8()
 
   # la Bayesian Optimization con el semillerio dentro
   ht <- HT_tuning_semillerio(
     semillerio = 50, # semillerio dentro de la Bayesian Optim
-    bo_iteraciones = 10  # iteraciones inteligentes, apenas 10
+    bo_iteraciones = 20  # iteraciones inteligentes, apenas 10
   )
 
 
@@ -522,6 +527,6 @@ wf_SEMI_ago_orden227 <- function( pnombrewf )
 # Aqui comienza el programa
 
 # llamo al workflow con future = 202108
-wf_SEMI_ago_orden227()
+wf_SEMI_ago_orden2()
 
 
